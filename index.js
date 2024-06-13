4a41c1042a9269b46373729e4b2dbd8693a9410b
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-13 02:22:08 +0200

    updated the js file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let inputElements = document.getElementsByClassName("input-value");
+let feetValue = document.querySelector("#feet-value");
+let meterValue = document.querySelector("#meter-value");
+let userInputValue = document.querySelector("#input-el");
 
+function convertNow() {
+  let value = userInputValue.value;
+  for (let i = 0; i < inputElements.length; i++) {
+    inputElements[i].textContent = value;
+  }
+}
+
+const convertButton = document.querySelector("#convert-btn");
+convertButton.addEventListener("click", convertNow);

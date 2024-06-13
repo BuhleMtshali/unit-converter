d99b9101920a4e227ef92e71b11be6c0a892fe9d
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-13 02:34:52 +0200

    updated the javascript file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let inputElements = document.getElementsByClassName("input-value");
+let inputElements = document.getElementsByClassName("input-value"); //this picks up all the elements with the same class
 let feetValue = document.querySelector("#feet-value");
 let meterValue = document.querySelector("#meter-value");
 let userInputValue = document.querySelector("#input-el");
@@ -6,8 +6,13 @@ let userInputValue = document.querySelector("#input-el");
 function convertNow() {
   let value = userInputValue.value;
   for (let i = 0; i < inputElements.length; i++) {
+    //we used a loop here because we were rendering more than one change
     inputElements[i].textContent = value;
   }
+  let feetToMeter = parseFloat(value * 3.281);
+  feetValue.textContent = feetToMeter.toFixed(2);
+  let meterToFeet = parseFloat(value / 3.281);
+  meterValue.textContent = meterToFeet.toFixed(2);
 }
 
 const convertButton = document.querySelector("#convert-btn");
